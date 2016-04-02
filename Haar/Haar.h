c91ab96b1c6b53056ed3406393f3d344c16c2d33
Matt G <matt.gaikema@tamu.edu>
2016-04-02 17:59:14 -0500

    Initial commit

---
+++ Haar/Haar.h
@@ -0,0 +1,15 @@
+#ifndef HAAR_H
+#define HAAR_H
+
+#pragma once
+class Haar
+{
+public:
+	Haar();
+	~Haar();
+
+	static double phi(double x);
+	static double psi(double x);
+};
+
+#endif
---
+++ Haar/Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include "Haar.h"
+
+using namespace std;
+
+int main(){
+	cout << Haar::phi(2.5) << endl;
+
+	vector<vector<double>> a;
+	vector<vector<double>> b;
+}
