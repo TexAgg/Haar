585c6c1fa3f4167d7f1e9889db67b3ff0f2491bd
Matt G <matt.gaikema@tamu.edu>
2016-04-03 17:46:00 -0500

    Find Coefficients

--- Haar/Main.cpp
+++ Haar/Main.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
 #include <vector>
+#include<fstream>
 #include "Haar.h"
-#include "Deconstructed.h"
 
 using namespace std;
 
 int main(){
-	cout << Haar::phi(2.5) << endl;
+	vector<double> a2 = { 0.5, 2, 2.5, -1.5 };
+	vector<double> b2 = { -1.5, -1, 0.5, -0.5 };
 
-	Deconstructed h(3);
+	vector<double> a3(2 * a2.size());
 
-	h.a.reserve(3);
-	h.b.reserve(3);
+	for (int k = 0; k < a2.size(); k++){
+		int l = 2 * k;
+		a3[l] = a2[k] + b2[k];
+		l++;
+		a3[l] = a2[k] - b2[k];
+	}
 
-	h.a.push_back({});
-	h.b.push_back({});
-	h.a.push_back({ 3 / 2.0, -1 });
-	h.b.push_back({ -1, -3 / 2.0 });
-	h.b.push_back({ -3 / 2.0, -3 / 2.0, -1 / 2.0, -1 / 2.0 });
+	ofstream outfile;
+	outfile.open("coefficient.csv");
 
-	h.subscripts(2);
-	
-	for (auto k : h.a[2]){
-		cout << k << ',';
+	for (auto k : a3){
+		outfile << k << ",";
 	}
 }
