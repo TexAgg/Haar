1af0141911592f43b509e4050f48f528dd48ad46
Matt G <matt.gaikema@tamu.edu>
2016-04-02 21:17:20 -0500

    Do math

--- Haar/Deconstructed.cpp
+++ Haar/Deconstructed.cpp
@@ -4,8 +4,9 @@ void Deconstructed::subscripts(){
 	for (int j = 1; j < size; j++){
 		std::vector<double> temp;
 		for (int k = 0; ; k++){
-			if (true){
-
+			for (int l = 0; l < k; l++){
+				if (l % 2 * k == 0){}
+					// what am i doing
 			}
 		}
 	}
