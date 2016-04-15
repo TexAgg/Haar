dab52ce81ceb8557eb60eb236306cdb937a405e5
Matt G <matt.gaikema@tamu.edu>
2016-04-15 14:20:28 -0500

    change everything

--- Haar/Main.cpp
+++ Haar/Main.cpp
@@ -1,9 +1,42 @@
 #include <iostream>
 #include <vector>
+#include <math.h>
 #include<fstream>
-#include "Haar.h"
+#include <algorithm>
 
 using namespace std;
+
+double phi(double x) {
+	if (x >= 0 && x < 1)
+		return 1;
+	else
+		return 0;
+}
+
+double psi(double x) {
+	return phi(2 * x) - phi(2 * x - 1);
+}
+
+/**
+	Calculate the discrete haar wavelet transform
+*/
+vector<double> discrete_haar_transform(vector<double> input) {
+	// Assumes that input.size() = 2^n
+	vector<double> output(input.size());
+
+	// Bit shift
+	for (int length = input.size() >> 1;; length >>= 1) {
+		for (int i = 0; i < length; i++) {
+			int sum = input[i * 2] + input[i * 2 + 1];
+			int difference = input[i * 2] - input[i * 2 + 1];
+			output[i] = sum;
+			output[length + i] = difference;
+		}
+		if (length == 1)
+			return output;
+		copy_n(output.begin(), length << 1, input.begin());
+	}
+}
 
 int main() {
 	vector<double> a2 = { 0.5, 2, 2.5, -1.5 };
@@ -24,6 +57,8 @@ int main() {
 	outfile.open("coefficient.csv");
 	for (int k = 0; k < a3.size() - 1; k++){
 		outfile << a3[k] << ",";
+		cout << a3[k] << ",";
 	}
 	outfile << a3[a3.size() - 1];
+	cout << a3[a3.size() - 1] << endl;
 }
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Haar
 Demonstrating the algorithm for reconstructing a function from its Haar coefficients.
+See [here](https://en.wikipedia.org/wiki/Discrete_wavelet_transform#Example_of_above_code).
